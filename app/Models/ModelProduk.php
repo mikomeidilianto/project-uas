7854e0fa23ca0b132db236ac78744500bb367a3c
Miko Meidilianto <mikomeidilianto@gmail.com>
2024-12-06 13:34:36 +0700

    tes branch

--- app/Models/ModelProduk.php
+++ app/Models/ModelProduk.php
@@ -19,10 +19,11 @@ class ModelProduk extends Model
     {
         // Melakukan join antara tabel 'products' dan 'categories' berdasarkan category_id
         return $this->db->table('products')
-            ->join('categories', 'categories.id = products.category_id', 'left')  // 'left' join agar semua produk ditampilkan meskipun tanpa kategori
-            ->select('products.*, categories.name AS category_name')  // Memilih semua kolom dari products dan 'name' dari categories
-            ->get(4)
-            ->getResultArray();
+        ->join('categories', 'categories.id = products.category_id', 'left')
+        ->select('products.*, categories.name AS category_name')
+        ->limit(4) // Membatasi hasil hanya 4 data
+        ->get()
+        ->getResultArray();
     }
     
      //Tambah data

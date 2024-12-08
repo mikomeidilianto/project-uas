21b8a0e01611fa445351b52b3c9cc977da1cafd3
Miko Meidilianto <mikomeidilianto@gmail.com>
2024-12-09 01:30:51 +0700

    login admin

--- app/Views/admin/v_template.php
+++ app/Views/admin/v_template.php
@@ -1,4 +1,3 @@
-
 <!--
 =========================================================
 * Soft UI Dashboard 3 - v1.1.0
@@ -186,7 +185,7 @@
         </li>
         
         <li class="nav-item">
-          <a class="nav-link  " href="<?= base_url('admin/Login') ?>">
+          <a class="nav-link  " href="<?= base_url('admin/Auth/logout') ?>">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>document</title>
